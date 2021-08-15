ae8f4d55cd63d778cfc4bb8627f4a88679b0e079
James A. Brannan <jamesabrannan@hushmail.com>
2021-08-14 22:15:44 -0400

    initial checkin

---
+++ src/main/java/com/dynamicpdf/api/examples/CloudApiExamples.java
@@ -0,0 +1,13 @@
+package com.dynamicpdf.api.examples;
+
+public class CloudApiExamples {
+
+	public static String BASE_URL = "https://api.dynamicpdf.com/v1.0";
+	
+	public static void main(String[] args) {
+		
+		PdfInfoExample.PdfExampleOne(args[0], args[1]);
+
+	}
+
+}
