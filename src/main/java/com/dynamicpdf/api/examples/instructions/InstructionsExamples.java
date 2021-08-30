3df51659e44418cc7845cafad54e22c0a447f168
James A. Brannan <jamesabrannan@hushmail.com>
2021-08-30 12:53:10 -0400

    added outlines

--- src/main/java/com/dynamicpdf/api/examples/instructions/InstructionsExamples.java
+++ src/main/java/com/dynamicpdf/api/examples/instructions/InstructionsExamples.java
@@ -9,15 +9,20 @@ import com.dynamicpdf.api.Aes256Security;
 import com.dynamicpdf.api.DlexResource;
 import com.dynamicpdf.api.Font;
 import com.dynamicpdf.api.FormField;
+import com.dynamicpdf.api.GoToAction;
 import com.dynamicpdf.api.ImageResource;
 import com.dynamicpdf.api.LayoutDataResource;
+import com.dynamicpdf.api.Outline;
 import com.dynamicpdf.api.PageInput;
+import com.dynamicpdf.api.PageZoom;
 import com.dynamicpdf.api.Pdf;
+import com.dynamicpdf.api.PdfInput;
 import com.dynamicpdf.api.PdfResource;
 import com.dynamicpdf.api.PdfResponse;
 import com.dynamicpdf.api.RgbColor;
 import com.dynamicpdf.api.elements.ElementPlacement;
 import com.dynamicpdf.api.elements.PageNumberingElement;
+import com.dynamicpdf.api.elements.TextElement;
 import com.dynamicpdf.api.util.PrettyPrintUtility;
 
 public class InstructionsExamples {
@@ -168,6 +173,99 @@ public class InstructionsExamples {
 				pdf.setSecurity(sec);
 				return pdf;
 			}
+			
+			
+			public static Pdf AddOutlinesExistingPdf(String basePath) {
+				Pdf pdf = new Pdf();
+				pdf.setAuthor("John Doe");
+				pdf.setTitle("Existing Pdf Example");
+
+				PdfResource resource1 = new PdfResource(basePath + "/DocumentA100.pdf");
+				PdfInput input1 = pdf.addPdf(resource1);
+				input1.setId("document2");
+
+				PdfResource resource2 = new PdfResource(basePath + "/Invoice.pdf");
+				PdfInput input2 = pdf.addPdf(resource2);
+				input2.setId("invoice");
+
+				Outline rootOutline = new Outline("Root Outline");
+				rootOutline.setExpanded(true);
+
+				Outline outline1 = new Outline("DocumentA 100");
+				outline1.setExpanded(true);
+
+				GoToAction linkTo2 = new GoToAction(input1);
+				linkTo2.setPageOffset(0);
+				linkTo2.setPageZoom(PageZoom.FITPAGE);
+				outline1.setAction(linkTo2);
+
+				Outline outline2 = new Outline("Invoice");
+				outline2.setExpanded(true);
+
+				GoToAction linkTo = new GoToAction(input2);
+				outline2.setAction(linkTo);
+
+				rootOutline.getChildren().add(outline1);
+				rootOutline.getChildren().add(outline2);
+
+				pdf.getOutlines().add(rootOutline);
+				return pdf;
+
+			}
+			
+			
+			
+			  public static Pdf AddOutlinesForNewPdf()
+		        {
+		            String name = "TextElement";
+		            Pdf pdf = new Pdf();
+		            pdf.setAuthor("John Doe");
+		            pdf.setTitle("Sample Pdf");
+
+		            PageInput pageInput = pdf.addPage();
+		            pageInput.setId("page1");
+		            TextElement element = new TextElement("Hello World 1", ElementPlacement.TOPCENTER);
+		            pageInput.getElements().add(element);
+
+		            PageInput pageInput1 = pdf.addPage();
+		            pageInput1.setId("page2");
+		            TextElement element1 = new TextElement("Hello World 2", ElementPlacement.TOPCENTER);
+		            pageInput1.getElements().add(element1);
+
+		            PageInput pageInput2 = pdf.addPage();
+		            pageInput2.setId("page3");
+		            TextElement element2 = new TextElement("Hello World 3", ElementPlacement.TOPCENTER);
+		            pageInput2.getElements().add(element2);
+
+
+		            Outline rootOutline = new Outline("Root Outline");
+		            rootOutline.setExpanded(true);
+
+		            Outline outline = new Outline("Page 1");
+		            outline.setExpanded(true);
+		            GoToAction linkTo = new GoToAction(pageInput);
+		            outline.setAction(linkTo);
+
+		            Outline outline2 = new Outline("Page 2");
+		            outline2.setExpanded(true);
+		            GoToAction linkTo1 = new GoToAction(pageInput1);
+		            outline2.setAction(linkTo1);
+
+		            Outline outline3 = new Outline("Page 3");
+		            outline3.setExpanded(true);
+		            GoToAction linkTo3 = new GoToAction(pageInput2);
+		            outline3.setAction(linkTo3);
+
+		            rootOutline.getChildren().add(outline);
+		            rootOutline.getChildren().add(outline2);
+		            rootOutline.getChildren().add(outline3);
+
+		            pdf.getOutlines().add(rootOutline);
+
+		            return pdf;
+		        }
+
+			
 
 			public static void printOut(Pdf pdf, String apiKey, String basePath, String outputFile)
 			{
@@ -190,18 +288,21 @@ public class InstructionsExamples {
 				}
 			}
 			
-
+			
 	public static void main(String[] args) {
 		
 		Pdf exampleOne = InstructionsExamples.topLevelMetaData();
-		InstructionsExamples.printOut(exampleOne, args[0], args[1], "c-sharp-top-level-metadata.pdf");
+		InstructionsExamples.printOut(exampleOne, args[0], args[1], "java-top-level-metadata.pdf");
 		Pdf exampleTwo = InstructionsExamples.SecurityExample(args[1]);
-		InstructionsExamples.printOut(exampleTwo, args[0], args[1], "c-sharp-security.pdf");
+		InstructionsExamples.printOut(exampleTwo, args[0], args[1], "java-security.pdf");
 		Pdf exampleThree = InstructionsExamples.MergingExample(args[1]);
-		InstructionsExamples.printOut(exampleThree, args[0], args[1], "c-sharp-merging.pdf");
-
+		InstructionsExamples.printOut(exampleThree, args[0], args[1], "java-merging.pdf");
 		Pdf exampleFour = InstructionsExamples.FormFieldsExample();
-		InstructionsExamples.printOut(exampleFour, args[0], args[1], "c-sharp-fonts.pdf");
+		InstructionsExamples.printOut(exampleFour, args[0], args[1], "java-fonts.pdf");
+		Pdf exampleFive = InstructionsExamples.AddOutlinesForNewPdf();
+		InstructionsExamples.printOut(exampleFive, args[0], args[1], "java-outline-create.pdf");
+		Pdf exampleSix = InstructionsExamples.AddOutlinesExistingPdf(args[1]);
+		InstructionsExamples.printOut(exampleSix, args[0], args[1], "java-outline-existing.pdf");
 	}
 
 }
