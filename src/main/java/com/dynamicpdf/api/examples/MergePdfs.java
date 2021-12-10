c464fbd200e6ebe4e51ea89ad6869e6ad7645baa
James A. Brannan <jamesabrannan@hushmail.com>
2021-12-10 17:50:42 -0500

    Getting Started and Merge Pdfs

---
+++ src/main/java/com/dynamicpdf/api/examples/MergePdfs.java
@@ -0,0 +1,59 @@
+package com.dynamicpdf.api.examples;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import com.dynamicpdf.api.Pdf;
+import com.dynamicpdf.api.PdfInput;
+import com.dynamicpdf.api.PdfResource;
+import com.dynamicpdf.api.PdfResponse;
+
+
+public class MergePdfs {
+
+	public static void main(String[] args) {
+		MergePdfs.Run("DP.rGTirmTqaXPuS7Vm+pUTZiiBuooF2kj6aMaAOqN5Sli6RaP1KyjmTHFO",
+				"C:/temp/dynamicpdf-api-samples/");
+
+	}
+
+	public static void Run(String apiKey, String basePath) {
+
+		// create new pdf instance and set api key
+		Pdf pdf = new Pdf();
+		pdf.setApiKey(apiKey);
+
+		// add pdf from local file system where specify that only one page
+		// from DocumentA is to be merged.
+
+		PdfInput pdfInput = pdf.addPdf(new PdfResource(basePath + "DocumentA.pdf"));
+		pdfInput.setStartPage(1);
+		pdfInput.setPageCount(1);
+		
+		// add all of DocumentB to the PDF to be created
+		
+		pdf.addPdf(new PdfResource(basePath + "DocumentB.pdf"));
+		
+		// add DocumentC from the cloud in Resource Manager
+		
+		pdf.addPdf("samples/merge-pdfs-pdf-endpoint/DocumentC.pdf");
+		
+		// call the endpoint and return the results
+
+		PdfResponse pdfResponse = pdf.process();
+
+		// if the response is successful, save merged pdf to file. if error, then
+		// printout JSON error
+
+		if (pdfResponse.getIsSuccessful()) {
+			try {
+				FileUtils.writeByteArrayToFile(new File(basePath + "merge-pdfs-output.pdf"), 
+						pdfResponse.getContent());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println(pdfResponse.getErrorJson());
+		}
+	}
+}
--- src/main/java/com/dynamicpdf/api/gettingstarted/GettingStartedInFive.java
+++ src/main/java/com/dynamicpdf/api/gettingstarted/GettingStartedInFive.java
@@ -1,33 +1,30 @@
 package com.dynamicpdf.api.gettingstarted;
 
-
 import java.io.File;
 import java.io.IOException;
-
 import org.apache.commons.io.FileUtils;
-
+import com.dynamicpdf.api.DlexLayout;
 import com.dynamicpdf.api.LayoutDataResource;
-import com.dynamicpdf.api.Pdf;
 import com.dynamicpdf.api.PdfResponse;
 
 public class GettingStartedInFive {
 
 	public static void main(String[] args) {
-        Pdf pdf = new Pdf();
-        pdf.setApiKey("DP.poEtD7F5tD1Ulp3qPcolUFaCcQFxWOvuNUqm/WragUdOSaAesnu3L6XE");
-        pdf.setAuthor("John Doe");
-        pdf.setTitle("Getting Started In Five Minutes");
-        LayoutDataResource layoutDataResource = new LayoutDataResource("C:/holding/instructions-examples/getting-started-data.json");
-        pdf.addDlex("samples/shared/dlex/getting-started.dlex", layoutDataResource);
-        PdfResponse pdfResponse = pdf.process();
-	    
-        try {
-			FileUtils.writeByteArrayToFile(new File("C:/holding/instructions-examples/getting-started-output.pdf"), 
+		GettingStartedInFive.Run("DP.V9z4rPOYV+b/je9WDNUtED6md3H1i4huSxNoMIXKicf/TYJRxT5H176B",
+				"C:/temp/dynamicpdf-api-samples/");
+	}
+
+	public static void Run(String apiKey, String basePath) {
+		LayoutDataResource layoutDataResource = new LayoutDataResource(basePath + "getting-started.json");
+		DlexLayout dlexLayout = new DlexLayout("samples/getting-started/getting-started.dlex", layoutDataResource);
+		dlexLayout.setApiKey(apiKey);
+		PdfResponse pdfResponse = dlexLayout.process();
+
+		try {
+			FileUtils.writeByteArrayToFile(new File(basePath + "getting-started-output.pdf"),
 					pdfResponse.getContent());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
