5ec070462cf0863092f23444491927fd41a19a82
James A. Brannan <jamesabrannan@hushmail.com>
2023-08-01 19:04:49 -0400

    Update CreatingPdfDlexLayout.java

--- src/main/java/com/dynamicpdf/api/examples/CreatingPdfDlexLayout.java
+++ src/main/java/com/dynamicpdf/api/examples/CreatingPdfDlexLayout.java
@@ -14,21 +14,23 @@ public class CreatingPdfDlexLayout {
 	
 	public static void main(String[] args) {
 		CreatingPdfDlexLayout.Run("DP.xxxx--api--key--xxx",
-				"C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/");
+				"C:/temp/dynamicpdf-api-samples/error-handling-example/");
 	}
 
 	public static void Run(String apiKey, String basePath) {
 
 		//get the local layoutdata
-		LayoutDataResource layoutData = new LayoutDataResource(basePath + "create-pdf-dlex-layout.json");
+		LayoutDataResource layoutData = new LayoutDataResource(basePath + "SimpleReportWithCoverPage.json");
 		
 		//load layoutdata and cloud dlex
-		DlexLayout dlexEndpoint = new DlexLayout("samples/creating-pdf-dlex-layout-endpoint/create-pdf-dlex-layout.dlex", layoutData);
+		DlexLayout dlexEndpoint = new DlexLayout("samples/error-handling-example/SimpleReportWithCoverPage.dlex", layoutData);
 		dlexEndpoint.setApiKey(apiKey);
 		
 		//call the dlex-layout endpoint and get response
 		PdfResponse response = dlexEndpoint.process();
 
+		System.out.println("Status: " + response.getStatusCode());	
+		
 		//if successful save as file
 		if (response.getIsSuccessful()) {
 			try {
