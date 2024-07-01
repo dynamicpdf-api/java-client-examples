f5cb94d68dff3e9d04d135d9b203f806bdaaf619
James A. Brannan <jamesabrannan@hushmail.com>
2024-07-01 11:22:49 -0400

    updated java pdf info endpoint example

--- src/main/java/com/dynamicpdf/api/examples/GetPdfInfo.java
+++ src/main/java/com/dynamicpdf/api/examples/GetPdfInfo.java
@@ -6,22 +6,23 @@ import com.dynamicpdf.api.PdfInfoResponse;
 import com.dynamicpdf.api.PdfResource;
 
 public class GetPdfInfo {
-	
-	// https://cloud.dynamicpdf.com/docs/tutorials/cloud-api/pdf-info/tutorial-pdf-info
-	
+
+
 	public static void main(String[] args) {
 		GetPdfInfo.Run(DynamicPdfCloudApiExamples.API_KEY, DynamicPdfCloudApiExamples.BASE_DIR + "/get-pdf-info-pdf-info-endpoint/");
 
 	}
 
 	public static void Run(String apiKey, String basePath) {
-		 PdfInfo pdfInfo = new PdfInfo(new PdfResource(basePath + "fw4.pdf"));
-         pdfInfo.setApiKey(apiKey);
-         PdfInfoResponse response = pdfInfo.process();
-         if(response.getIsSuccessful()) {
-        	 System.out.println(response.getJsonContent());
-         } else {
-        	 System.out.println(response.getErrorJson());
-         }
+		PdfResource resource = new PdfResource(basePath + "fw4.pdf");
+		PdfInfo pdfInfo = new PdfInfo(resource);
+		pdfInfo.setApiKey(apiKey);
+		PdfInfoResponse response = pdfInfo.process();
+		
+		if(response.getIsSuccessful()) {
+			System.out.println(response.getJsonContent());
+		} else {
+			System.out.println(response.getErrorJson());
+		}
 	} 
 }
